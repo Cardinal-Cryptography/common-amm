2c61a4265d1f4158696f59ce8126f7b178526ad1
Maciej Żelaszczyk <maciek.zelaszczyk@gmail.com>
2023-05-29 15:46:20 +0200

    Naming

--- e2e-tests/src/test/fee.rs
+++ e2e-tests/src/test/fee.rs
@@ -26,8 +26,8 @@ pub async fn factory_contract_set_up_correctly() -> Result<()> {
     } = setup_test().await?;
 
     let factory_contract = contracts.factory_contract;
-    let zero_account_id = ink_primitives::AccountId::from(ZERO_ADDRESS);
-    let regular_account_id = regular.account_id().to_account_id();
+    let zero_account = ink_primitives::AccountId::from(ZERO_ADDRESS);
+    let regular_account = regular.account_id().to_account_id();
 
     let recipient = factory_contract.fee_to(&wealthy_connection).await??;
     let setter = factory_contract
@@ -37,8 +37,8 @@ pub async fn factory_contract_set_up_correctly() -> Result<()> {
         .all_pairs_length(&wealthy_connection)
         .await??;
 
-    assert!(recipient == zero_account_id);
-    assert!(setter == regular_account_id);
+    assert!(recipient == zero_account);
+    assert!(setter == regular_account);
     assert!(all_pairs_length == 0);
 
     Ok(())
@@ -60,11 +60,11 @@ pub async fn set_fee() -> Result<()> {
         ..
     } = contracts;
 
-    let zero_account_id = ink_primitives::AccountId::from(ZERO_ADDRESS);
+    let zero_account = ink_primitives::AccountId::from(ZERO_ADDRESS);
     let wealthy_recipient = factory_contract.fee_to(&wealthy_connection).await??;
-    let regular_account_id = regular.account_id().to_account_id();
+    let regular_account = regular.account_id().to_account_id();
 
-    assert!(wealthy_recipient == zero_account_id);
+    assert!(wealthy_recipient == zero_account);
 
     ensure!(
         factory_contract
@@ -75,12 +75,12 @@ pub async fn set_fee() -> Result<()> {
     );
 
     factory_contract
-        .set_fee_to(&regular_connection, regular_account_id)
+        .set_fee_to(&regular_connection, regular_account)
         .await?;
 
     let regular_recipient = factory_contract.fee_to(&regular_connection).await??;
 
-    assert!(regular_recipient == regular_account_id);
+    assert!(regular_recipient == regular_account);
 
     Ok(())
 }
@@ -98,31 +98,31 @@ pub async fn set_fee_setter() -> Result<()> {
 
     let factory_contract = contracts.factory_contract;
 
-    let wealthy_account_id = wealthy.account_id().to_account_id();
-    let regular_account_id = regular.account_id().to_account_id();
+    let wealthy_account = wealthy.account_id().to_account_id();
+    let regular_account = regular.account_id().to_account_id();
     let setter_before = factory_contract
         .fee_to_setter(&wealthy_connection)
         .await??;
 
-    assert!(setter_before == regular_account_id);
+    assert!(setter_before == regular_account);
 
     ensure!(
         factory_contract
-            .set_fee_to_setter(&wealthy_connection, wealthy_account_id)
+            .set_fee_to_setter(&wealthy_connection, wealthy_account)
             .await
             .is_err(),
         "Call should have errored out - caller is not fee setter!"
     );
 
     factory_contract
-        .set_fee_to_setter(&regular_connection, wealthy_account_id)
+        .set_fee_to_setter(&regular_connection, wealthy_account)
         .await?;
 
     let setter_after = factory_contract
         .fee_to_setter(&regular_connection)
         .await??;
 
-    assert!(setter_after == wealthy_account_id);
+    assert!(setter_after == wealthy_account);
 
     Ok(())
 }
