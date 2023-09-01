0b9a2871e34c5be9b670b3e98188014020bceda7
deuszx <mateusz.gorski@cardinals.cc>
2023-09-01 14:47:59 +0200

    Fix Factory contract.

--- uniswap-v2/logics/impls/factory/factory.rs
+++ uniswap-v2/logics/impls/factory/factory.rs
@@ -1,105 +1,14 @@
 pub use crate::{
-    ensure,
     impls::factory::*,
     traits::factory::*,
 };
-use ink::{
-    env::hash::Blake2x256,
-    primitives::Hash,
-};
 use openbrush::{
     modifier_definition,
-    modifiers,
     traits::{
         AccountId,
-        AccountIdExt,
         Storage,
     },
 };
-
-impl<T> Factory for T
-where
-    T: Internal,
-    T: Storage<data::Data>,
-{
-    default fn all_pairs(&self, pid: u64) -> Option<AccountId> {
-        self.data::<data::Data>().all_pairs.get(&pid)
-    }
-
-    default fn all_pairs_length(&self) -> u64 {
-        self.data::<data::Data>().all_pairs_length
-    }
-
-    default fn pair_contract_code_hash(&self) -> Hash {
-        self.data::<data::Data>().pair_contract_code_hash
-    }
-
-    default fn create_pair(
-        &mut self,
-        token_a: AccountId,
-        token_b: AccountId,
-    ) -> Result<AccountId, FactoryError> {
-        ensure!(token_a != token_b, FactoryError::IdenticalAddresses);
-        let token_pair = if token_a < token_b {
-            (token_a, token_b)
-        } else {
-            (token_b, token_a)
-        };
-        ensure!(!token_pair.0.is_zero(), FactoryError::ZeroAddress);
-        ensure!(
-            self.data::<data::Data>()
-                .get_pair
-                .get(&token_pair)
-                .is_none(),
-            FactoryError::PairExists
-        );
-
-        let salt = Self::env().hash_encoded::<Blake2x256, _>(&token_pair);
-        let pair_contract = self._instantiate_pair(salt.as_ref(), token_pair.0, token_pair.1)?;
-
-        self.data::<data::Data>()
-            .get_pair
-            .insert(&(token_pair.0, token_pair.1), &pair_contract);
-        self.data::<data::Data>()
-            .get_pair
-            .insert(&(token_pair.1, token_pair.0), &pair_contract);
-
-        self._add_new_pair(pair_contract);
-
-        self._emit_create_pair_event(
-            token_pair.0,
-            token_pair.1,
-            pair_contract,
-            self.all_pairs_length(),
-        );
-
-        Ok(pair_contract)
-    }
-
-    #[modifiers(only_fee_setter)]
-    default fn set_fee_to(&mut self, fee_to: AccountId) -> Result<(), FactoryError> {
-        self.data::<data::Data>().fee_to = fee_to;
-        Ok(())
-    }
-
-    #[modifiers(only_fee_setter)]
-    default fn set_fee_to_setter(&mut self, fee_to_setter: AccountId) -> Result<(), FactoryError> {
-        self.data::<data::Data>().fee_to_setter = fee_to_setter;
-        Ok(())
-    }
-
-    default fn fee_to(&self) -> AccountId {
-        self.data::<data::Data>().fee_to
-    }
-
-    default fn fee_to_setter(&self) -> AccountId {
-        self.data::<data::Data>().fee_to_setter
-    }
-
-    default fn get_pair(&self, token_a: AccountId, token_b: AccountId) -> Option<AccountId> {
-        self.data::<data::Data>().get_pair.get(&(token_a, token_b))
-    }
-}
 
 pub trait Internal {
     fn _emit_create_pair_event(
