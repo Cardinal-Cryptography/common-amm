abf524116a547f125459931ea905481445ca4cb6
Michal Handzlik <4hansu@gmail.com>
2023-10-10 17:26:39 +0200

    Wnative contract

--- amm/contracts/wnative/lib.rs
+++ amm/contracts/wnative/lib.rs
@@ -1,34 +1,97 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
-#![feature(min_specialization)]
 
-#[openbrush::contract]
+#[ink::contract]
 pub mod wnative {
-    use amm::traits::wnative::{
-        wnative_external,
-        Wnative,
+    use amm::traits::wnative::Wnative;
+    use ink::prelude::{
+        string::String,
+        vec::Vec,
     };
-    use ink::{
-        codegen::{
-            EmitEvent,
-            Env,
-        },
-        prelude::vec::Vec,
-    };
-    use openbrush::{
-        contracts::psp22::extensions::metadata::*,
-        traits::{
-            Storage,
-            String,
-        },
+    use psp22::{
+        PSP22Data,
+        PSP22Error,
+        PSP22Event,
+        PSP22Metadata,
+        PSP22,
     };
 
-    #[ink(event)]
-    pub struct Transfer {
-        #[ink(topic)]
-        from: Option<AccountId>,
-        #[ink(topic)]
-        to: Option<AccountId>,
-        value: Balance,
+    #[ink(storage)]
+    #[derive(Default)]
+    pub struct WnativeContract {
+        data: PSP22Data,
+    }
+
+    impl WnativeContract {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self::default()
+        }
+
+        fn emit_events(&self, events: Vec<PSP22Event>) {
+            for event in events {
+                match event {
+                    PSP22Event::Transfer { from, to, value } => {
+                        self.env().emit_event(Transfer { from, to, value })
+                    }
+                    PSP22Event::Approval {
+                        owner,
+                        spender,
+                        amount,
+                    } => {
+                        self.env().emit_event(Approval {
+                            owner,
+                            spender,
+                            amount,
+                        })
+                    }
+                }
+            }
+        }
+
+        /// For e2e testing purposes only. Do not use in production!
+        #[cfg(feature = "e2e-tests")]
+        #[ink(message)]
+        pub fn terminate(&mut self) -> Result<(), PSP22Error> {
+            let caller = self.env().caller();
+            self.env().terminate_contract(caller)
+        }
+    }
+
+    impl Wnative for WnativeContract {
+        #[ink(message)]
+        fn deposit(&mut self) -> Result<(), PSP22Error> {
+            let events = self
+                .data
+                .mint(self.env().caller(), self.env().transferred_value())?;
+            Ok(self.emit_events(events))
+        }
+
+        #[ink(message)]
+        fn withdraw(&mut self, value: Balance) -> Result<(), PSP22Error> {
+            let caller = self.env().caller();
+            let events = self.data.burn(caller, value)?;
+            self.env()
+                .transfer(caller, value)
+                .map_err(|_| PSP22Error::Custom(String::from("Wrapper AZERO: withdraw failed")))?;
+            Ok(self.emit_events(events))
+        }
+    }
+
+    impl PSP22Metadata for WnativeContract {
+        #[ink(message)]
+        fn token_name(&self) -> Option<String> {
+            Some(String::from("Wrapped AZERO"))
+        }
+
+        #[ink(message)]
+        fn token_symbol(&self) -> Option<String> {
+            Some(String::from("WAZERO"))
+        }
+
+        #[ink(message)]
+        fn token_decimals(&self) -> u8 {
+            12
+        }
     }
 
     #[ink(event)]
@@ -37,79 +100,43 @@ pub mod wnative {
         owner: AccountId,
         #[ink(topic)]
         spender: AccountId,
-        value: Balance,
+        amount: u128,
     }
 
-    #[ink(storage)]
-    #[derive(Default, Storage)]
-    pub struct WnativeContract {
-        #[storage_field]
-        psp22: psp22::Data,
-        #[storage_field]
-        metadata: metadata::Data,
-    }
-
-    impl Wnative for WnativeContract {
-        #[ink(message)]
-        fn deposit(&mut self) -> Result<(), PSP22Error> {
-            let transfer_value = self.env().transferred_value();
-            let caller = self.env().caller();
-            self._mint_to(caller, transfer_value)
-        }
-
-        #[ink(message)]
-        fn withdraw(&mut self, amount: Balance) -> Result<(), PSP22Error> {
-            let caller = self.env().caller();
-            self._burn_from(caller, amount)?;
-            self.env()
-                .transfer(caller, amount)
-                .map_err(|_| PSP22Error::Custom(String::from("WNATIVE: transfer failed")))
-        }
-    }
-
-    impl PSP22Metadata for WnativeContract {
-        #[ink(message)]
-        fn token_name(&self) -> Option<String> {
-            self.metadata.name.clone()
-        }
-
-        #[ink(message)]
-        fn token_symbol(&self) -> Option<String> {
-            self.metadata.symbol.clone()
-        }
-
-        #[ink(message)]
-        fn token_decimals(&self) -> u8 {
-            self.metadata.decimals
-        }
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        value: u128,
     }
 
     impl PSP22 for WnativeContract {
         #[ink(message)]
-        fn total_supply(&self) -> Balance {
-            self._total_supply()
+        fn total_supply(&self) -> u128 {
+            self.data.total_supply()
         }
 
         #[ink(message)]
-        fn balance_of(&self, owner: AccountId) -> Balance {
-            self._balance_of(&owner)
+        fn balance_of(&self, owner: AccountId) -> u128 {
+            self.data.balance_of(owner)
         }
 
         #[ink(message)]
-        fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
-            self._allowance(&owner, &spender)
+        fn allowance(&self, owner: AccountId, spender: AccountId) -> u128 {
+            self.data.allowance(owner, spender)
         }
 
         #[ink(message)]
         fn transfer(
             &mut self,
             to: AccountId,
-            value: Balance,
-            data: Vec<u8>,
+            value: u128,
+            _data: ink::prelude::vec::Vec<u8>,
         ) -> Result<(), PSP22Error> {
-            let from = self.env().caller();
-            self._transfer_from_to(from, to, value, data)?;
-            Ok(())
+            let events = self.data.transfer(self.env().caller(), to, value)?;
+            Ok(self.emit_events(events))
         }
 
         #[ink(message)]
@@ -117,97 +144,43 @@ pub mod wnative {
             &mut self,
             from: AccountId,
             to: AccountId,
-            value: Balance,
-            data: Vec<u8>,
+            value: u128,
+            _data: ink::prelude::vec::Vec<u8>,
         ) -> Result<(), PSP22Error> {
-            let caller = self.env().caller();
-            let allowance = self._allowance(&from, &caller);
-
-            if allowance < value {
-                return Err(PSP22Error::InsufficientAllowance)
-            }
-
-            self._approve_from_to(from, caller, allowance - value)?;
-            self._transfer_from_to(from, to, value, data)?;
-            Ok(())
+            let events = self
+                .data
+                .transfer_from(self.env().caller(), from, to, value)?;
+            Ok(self.emit_events(events))
         }
 
         #[ink(message)]
-        fn approve(&mut self, spender: AccountId, value: Balance) -> Result<(), PSP22Error> {
-            let owner = self.env().caller();
-            self._approve_from_to(owner, spender, value)?;
-            Ok(())
+        fn approve(&mut self, spender: AccountId, value: u128) -> Result<(), PSP22Error> {
+            let events = self.data.approve(self.env().caller(), spender, value)?;
+            Ok(self.emit_events(events))
         }
 
         #[ink(message)]
         fn increase_allowance(
             &mut self,
             spender: AccountId,
-            delta_value: Balance,
+            delta_value: u128,
         ) -> Result<(), PSP22Error> {
-            let owner = self.env().caller();
-            self._approve_from_to(
-                owner,
-                spender,
-                self._allowance(&owner, &spender) + delta_value,
-            )
+            let events = self
+                .data
+                .increase_allowance(self.env().caller(), spender, delta_value)?;
+            Ok(self.emit_events(events))
         }
 
         #[ink(message)]
         fn decrease_allowance(
             &mut self,
             spender: AccountId,
-            delta_value: Balance,
+            delta_value: u128,
         ) -> Result<(), PSP22Error> {
-            let owner = self.env().caller();
-            let allowance = self._allowance(&owner, &spender);
-
-            if allowance < delta_value {
-                return Err(PSP22Error::InsufficientAllowance)
-            }
-
-            self._approve_from_to(owner, spender, allowance - delta_value)
-        }
-    }
-
-    impl psp22::Internal for WnativeContract {
-        fn _emit_transfer_event(
-            &self,
-            from: Option<AccountId>,
-            to: Option<AccountId>,
-            amount: Balance,
-        ) {
-            self.env().emit_event(Transfer {
-                from,
-                to,
-                value: amount,
-            });
-        }
-
-        fn _emit_approval_event(&self, owner: AccountId, spender: AccountId, amount: Balance) {
-            self.env().emit_event(Approval {
-                owner,
-                spender,
-                value: amount,
-            });
-        }
-    }
-
-    impl WnativeContract {
-        #[ink(constructor)]
-        pub fn new() -> Self {
-            let mut instance = Self::default();
-            instance.metadata.name = Some(String::from("Wrapped Native"));
-            instance.metadata.symbol = Some(String::from("WNATIVE"));
-            instance.metadata.decimals = 12;
-            instance
-        }
-        /// For e2e testing purposes only. Do not use in production!
-        #[cfg(feature = "e2e-tests")]
-        #[ink(message)]
-        pub fn terminate(&mut self) -> Result<(), PSP22Error> {
-            let caller = self.env().caller();
-            self.env().terminate_contract(caller)
+            let events = self
+                .data
+                .decrease_allowance(self.env().caller(), spender, delta_value)?;
+            Ok(self.emit_events(events))
         }
     }
 
@@ -219,12 +192,12 @@ pub mod wnative {
         fn register_works() {
             let wnative_contract = WnativeContract::new();
             assert_eq!(
-                wnative_contract.metadata.name,
-                Some(String::from("Wrapped Native"))
+                wnative_contract.token_name(),
+                Some(String::from("Wrapped AZERO"))
             );
             assert_eq!(
-                wnative_contract.metadata.symbol,
-                Some(String::from("WNATIVE"))
+                wnative_contract.token_symbol(),
+                Some(String::from("WAZERO"))
             );
         }
 
@@ -235,7 +208,7 @@ pub mod wnative {
             assert_eq!(deposit(&mut wnative_contract, 1000), Ok(()));
             let balance = wnative_contract.balance_of(accounts.alice);
             assert_eq!(balance, 1000, "balance not correct!");
-            let native_balance: Balance = wnative_contract.env().balance();
+            let native_balance: Balance = get_balance(contract_id());
             assert_eq!(native_balance, 1000, "native balance not correct!");
         }
 
@@ -243,10 +216,9 @@ pub mod wnative {
         fn test_withdraw() {
             let accounts = default_accounts();
             let mut wnative_contract = create_contract(1000);
-            assert_eq!(get_balance(wnative_contract.env().account_id()), 1000);
-            assert_eq!(
-                wnative_contract._mint_to(accounts.alice, 1000),
-                Ok(()),
+            assert_eq!(get_balance(contract_id()), 1000);
+            assert!(
+                wnative_contract.data.mint(accounts.alice, 1000).is_ok(),
                 "mint failed"
             );
             let wnative_balance = wnative_contract.balance_of(accounts.alice);
@@ -271,6 +243,15 @@ pub mod wnative {
             ink::env::test::set_caller::<Environment>(caller);
         }
 
+        fn contract_id() -> AccountId {
+            ink::env::test::callee::<ink::env::DefaultEnvironment>()
+        }
+
+        fn get_balance(account_id: AccountId) -> Balance {
+            ink::env::test::get_account_balance::<ink::env::DefaultEnvironment>(account_id)
+                .expect("Cannot get account balance")
+        }
+
         fn set_balance(account_id: AccountId, balance: Balance) {
             ink::env::test::set_account_balance::<ink::env::DefaultEnvironment>(account_id, balance)
         }
@@ -285,18 +266,9 @@ pub mod wnative {
             WnativeContract::new()
         }
 
-        fn contract_id() -> AccountId {
-            ink::env::test::callee::<ink::env::DefaultEnvironment>()
-        }
-
-        fn get_balance(account_id: AccountId) -> Balance {
-            ink::env::test::get_account_balance::<ink::env::DefaultEnvironment>(account_id)
-                .expect("Cannot get account balance")
-        }
-
         fn deposit(contract: &mut WnativeContract, amount: Balance) -> Result<(), PSP22Error> {
             let sender = ink::env::caller::<ink::env::DefaultEnvironment>();
-            let contract_id = contract.env().account_id();
+            let contract_id = contract_id();
             let sender_balance = get_balance(sender);
             let contract_balance = get_balance(contract_id);
             // ↓ doesn't work, is upstream issue: https://github.com/paritytech/ink/issues/1117
