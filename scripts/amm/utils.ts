235d7377ea77b256d6c0f800d07a904ea62882de
deuszx <mateusz.gorski@cardinals.cc>
2023-11-16 21:13:13 +0100

    Update scripts to reflect new paths & names

--- scripts/amm/utils.ts
+++ scripts/amm/utils.ts
@@ -67,7 +67,7 @@ export async function uploadCode(
   contractName: string,
 ): Promise<HexString> {
   const tokenContractRaw = JSON.parse(
-    fs.readFileSync(__dirname + `/../artifacts/` + contractName, 'utf8'),
+    fs.readFileSync(__dirname + `/../../artifacts/` + contractName, 'utf8'),
   );
   const tokenAbi = new Abi(tokenContractRaw);
   const _txHash = await new Promise(async (resolve, reject) => {
@@ -99,7 +99,7 @@ export async function estimateContractInit(
   sampleArgs: unknown[],
 ): Promise<WeightV2> {
   const contractRaw = JSON.parse(
-    fs.readFileSync(__dirname + `/../artifacts/` + contractName, 'utf8'),
+    fs.readFileSync(__dirname + `/../../artifacts/` + contractName, 'utf8'),
   );
   const contractAbi = new Abi(contractRaw);
   const { gasRequired } = (await api.call.contractsApi.instantiate(
