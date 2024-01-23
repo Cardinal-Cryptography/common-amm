37d8164ea87ffda065beec2e4f0ccfcf91986c81
deuszx <mateusz.gorski@cardinals.cc>
2024-01-23 12:32:56 +0100

    Test for 0xcd23b0692921a9c6c5589cbb82687e0275a540711c340e09fe3eb3666529f752

--- farm/tests/src/hats.rs
+++ farm/tests/src/hats.rs
@@ -302,3 +302,132 @@ fn claim_rewards_long_after_farm_ends(mut session: Session) {
         "Expected rewards don't change once the farm ends",
     );
 }
+
+#[drink::test]
+fn deposit_after_finish_doesnt_earn_rewards(mut session: Session) {
+    // Set up the necessary tokens.
+    // ICE - LP token
+    // WOOD and SAND - reward tokens
+    let ice = psp22::setup(&mut session, ICE.to_string(), ICE.to_string(), BOB);
+    let wood = psp22::setup(&mut session, WOOD.to_string(), WOOD.to_string(), BOB);
+    let sand = psp22::setup(&mut session, SAND.to_string(), SAND.to_string(), BOB);
+
+    const FARM_OWNER: AccountId32 = BOB;
+    const FARMER: AccountId32 = ALICE;
+
+    let farm = farm::setup(
+        &mut session,
+        ice.into(),
+        vec![wood.into(), sand.into()],
+        FARM_OWNER,
+    );
+
+    // Seed the farmer with some tokens to execute txns.
+    session
+        .sandbox()
+        .mint_into(ALICE, 1_000_000_000u128)
+        .unwrap();
+
+    // deposits lp token
+    let deposit_amount = 1000000;
+    inc_timestamp(&mut session);
+
+    // Deposit LP tokens as Alice, not Bob.
+    psp22::transfer(&mut session, ice.into(), alice(), deposit_amount, BOB).unwrap();
+    psp22::increase_allowance(
+        &mut session,
+        ice.into(),
+        farm.into(),
+        deposit_amount,
+        FARMER,
+    );
+    assert_eq!(
+        farm::deposit_to_farm(&mut session, &farm, deposit_amount, FARMER),
+        Ok(())
+    );
+
+    // Start the first farm
+    let now = get_timestamp(&mut session);
+    set_timestamp(&mut session, now);
+    let farm_duration = 100;
+    let farm_start = now + 10;
+    let farm_end = farm_start + farm_duration;
+    let rewards_amount = 100000000000000;
+    psp22::increase_allowance(
+        &mut session,
+        wood.into(),
+        farm.into(),
+        rewards_amount,
+        FARM_OWNER,
+    );
+    psp22::increase_allowance(
+        &mut session,
+        sand.into(),
+        farm.into(),
+        rewards_amount,
+        FARM_OWNER,
+    );
+    farm::start(
+        &mut session,
+        &farm,
+        farm_start,
+        farm_end,
+        vec![rewards_amount, rewards_amount],
+        FARM_OWNER,
+    )
+    .unwrap();
+
+    // set timestamp to farm end so users earn some reward in this farm but AND contract has balance
+    set_timestamp(&mut session, farm_end);
+
+    // There's one farmer that owns all LPs and participates in both farms for the whole duration.
+    // Owner withdrew WOOD tokens from the contract before the second farm started.
+    // So there are no WOOD rewards for the second farm.
+    let expected_rewards = Ok(vec![rewards_amount, rewards_amount]);
+    assert_eq!(
+        expected_rewards,
+        farm::query_unclaimed_rewards(&mut session, &farm, vec![0, 1], FARMER)
+    );
+    set_timestamp(&mut session, farm_end + farm_duration);
+    // Deposit to farm after it's finished.
+    psp22::increase_allowance(
+        &mut session,
+        ice.into(),
+        farm.into(),
+        deposit_amount,
+        FARM_OWNER,
+    );
+    assert_eq!(
+        farm::deposit_to_farm(&mut session, &farm, deposit_amount, FARM_OWNER),
+        Ok(())
+    );
+    inc_timestamp(&mut session);
+    psp22::increase_allowance(
+        &mut session,
+        ice.into(),
+        farm.into(),
+        deposit_amount,
+        FARM_OWNER,
+    );
+    farm::deposit_to_farm(&mut session, &farm, deposit_amount, FARM_OWNER).unwrap();
+    set_timestamp(&mut session, farm_end + farm_duration + farm_duration);
+    assert_eq!(
+        Ok(vec![0, 0]),
+        farm::claim_rewards(&mut session, &farm, vec![0, 1], FARM_OWNER),
+        "Rewards should not accrue after the farm ends"
+    );
+    // Move the timestmamp.
+    inc_timestamp(&mut session);
+    assert_eq!(
+        Ok(vec![0, 0]),
+        farm::claim_rewards(&mut session, &farm, vec![0, 1], FARM_OWNER),
+        "Rewards should not accrue after the farm ends"
+    );
+    inc_timestamp(&mut session);
+    farm::owner_withdraw(&mut session, &farm, wood.into(), FARM_OWNER).unwrap();
+    inc_timestamp(&mut session);
+    assert_eq!(
+        expected_rewards,
+        farm::claim_rewards(&mut session, &farm, vec![0, 1], FARMER)
+    );
+}
