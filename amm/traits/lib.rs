32281e0b86c8b912458eb3ca50821065c0cc1e21
JanKuczma <jan.kuczma@cardinals.cc>
2024-05-07 15:24:49 +0200

    fmt

--- amm/traits/lib.rs
+++ amm/traits/lib.rs
@@ -7,8 +7,8 @@ mod swap_callee;
 
 pub type Balance = <ink::env::DefaultEnvironment as ink::env::Environment>::Balance;
 
+pub use amm_helpers::math::MathError;
 pub use factory::{Factory, FactoryError};
 pub use pair::{Pair, PairError};
 pub use router::{Router, RouterError};
 pub use swap_callee::SwapCallee;
-pub use amm_helpers::math::MathError;
