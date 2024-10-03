6e256756508db8ead48224f6a1013bf03b596885
woocash2 <lukasgniecki@gmail.com>
2024-10-03 13:50:47 +0200

    v2 amount utils

--- amm/drink-tests/src/utils.rs
+++ amm/drink-tests/src/utils.rs
@@ -1023,6 +1023,142 @@ pub mod stable_swap {
     }
 }
 
+pub mod v2_amounts {
+    use primitive_types::U256;
+    use crate::pair_contract::{MathError, Pair as _,  PairError};
+    use super::*;
+
+    const PAIR_TRADING_FEE_DENOM: u128 = 1000;
+
+    struct PairParams {
+        token_0: AccountId,
+        token_1: AccountId,
+        reserve_0: u128,
+        reserve_1: u128,
+        fee: u128,
+    }
+
+    pub fn get_amount_out(
+        session: &mut Session<MinimalRuntime>,
+        pair: AccountId,
+        token_in: AccountId,
+        token_out: AccountId,
+        amount_in: u128,
+    ) -> Result<u128, MathError> {
+        let PairParams {
+            token_0,
+            token_1,
+            reserve_0,
+            reserve_1,
+            fee,
+        } = get_pair_params(session, pair);
+
+        let (reserve_in, reserve_out) = if token_0 == token_in && token_1 == token_out {
+            (reserve_0, reserve_1)
+        } else {
+            (reserve_1, reserve_0)
+        };
+
+        // Adjusts for fees paid in the `token_in`.
+        let amount_in_with_fee = casted_mul(amount_in, PAIR_TRADING_FEE_DENOM - fee);
+
+        let numerator = amount_in_with_fee
+            .checked_mul(reserve_out.into())
+            .ok_or(MathError::MulOverflow(13))?;
+
+        let denominator = casted_mul(reserve_in, PAIR_TRADING_FEE_DENOM)
+            .checked_add(amount_in_with_fee)
+            .ok_or(MathError::AddOverflow(2))?;
+
+        let amount_out: u128 = numerator
+            .checked_div(denominator)
+            .ok_or(MathError::DivByZero(7))?
+            .try_into()
+            .map_err(|_| MathError::CastOverflow(4))?;
+
+        Ok(amount_out)
+    }
+
+    pub fn get_amount_in(
+        session: &mut Session<MinimalRuntime>,
+        pair: AccountId,
+        token_in: AccountId,
+        token_out: AccountId,
+        amount_out: u128,
+    ) -> Result<u128, MathError> {
+        let PairParams {
+            token_0,
+            token_1,
+            reserve_0,
+            reserve_1,
+            fee,
+        } = get_pair_params(session, pair);
+
+        let (reserve_in, reserve_out) = if token_0 == token_in && token_1 == token_out {
+            (reserve_0, reserve_1)
+        } else {
+            (reserve_1, reserve_0)
+        };
+
+        let numerator = casted_mul(reserve_in, amount_out)
+            .checked_mul(PAIR_TRADING_FEE_DENOM.into())
+            .ok_or(MathError::MulOverflow(14))?;
+
+        let denominator = casted_mul(
+            reserve_out
+                .checked_sub(amount_out)
+                .ok_or(MathError::SubUnderflow(15))?,
+            PAIR_TRADING_FEE_DENOM - fee,
+        );
+
+        let amount_in: u128 = numerator
+            .checked_div(denominator)
+            .ok_or(MathError::DivByZero(8))?
+            .checked_add(1.into())
+            .ok_or(MathError::AddOverflow(3))?
+            .try_into()
+            .map_err(|_| MathError::CastOverflow(5))?;
+
+        Ok(amount_in)
+    }
+
+    fn get_pair_params(session: &mut Session<MinimalRuntime>, pair: AccountId) -> PairParams {
+        let contract = pair_contract::Instance::from(pair);
+        let token_0 = handle_contract_result(
+            session.query(
+                contract.get_token_0()
+            ).unwrap()
+        );
+        let token_1 = handle_contract_result(
+            session.query(
+                contract.get_token_1()
+            ).unwrap()
+        );
+        let (reserve_0, reserve_1, _) = handle_contract_result(
+            session.query(
+                contract.get_reserves()
+            ).unwrap()
+        );
+        let fee = handle_contract_result(
+            session.query(
+                contract.get_fee()
+            ).unwrap()
+        );
+
+        PairParams {
+            token_0,
+            token_1,
+            reserve_0,
+            reserve_1,
+            fee: fee as u128,
+        }
+    }
+
+    fn casted_mul(a: u128, b: u128) -> U256 {
+        U256::from(a) * U256::from(b)
+    }
+}
+
 pub fn get_timestamp(session: &mut Session<MinimalRuntime>) -> u64 {
     session.sandbox().get_timestamp()
 }
